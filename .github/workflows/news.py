c4284416aabb10b0e8bbae05c422505d9a3f422e
hailee911 <167476418+hailee911@users.noreply.github.com>
2024-10-28 10:50:48 +0900

    Update news.py

--- .github/workflows/news.py
+++ .github/workflows/news.py
@@ -1,14 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-import pyautogui
-import time
 import requests
 from bs4 import BeautifulSoup
-import random
 import csv
 # email 발송관련
 import smtplib
